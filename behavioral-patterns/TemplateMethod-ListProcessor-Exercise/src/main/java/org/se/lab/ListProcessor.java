9efbca7ad9e99d2acf21257c670df1b2da75c656
teiniker <egon.teiniker@fh-joanneum.at>
2017-03-30 11:41:19 +0200

    Initial commit.

---
+++ behavioral-patterns/TemplateMethod-ListProcessor-Exercise/src/main/java/org/se/lab/ListProcessor.java
@@ -0,0 +1,27 @@
+package org.se.lab;
+
+import java.util.List;
+
+public class ListProcessor
+{
+    /*
+     * Template Method
+     */
+    
+    public void process(List<String> list)
+    {
+        System.out.println("ListProcessor.process()");
+        
+        for(int i=0; i<list.size(); i++)
+        {
+        	String element = list.get(i).toUpperCase();
+            printElement(element); 
+            list.set(i, element);
+        }
+    }
+
+    protected void printElement(String element)
+    {
+        System.out.println("> " + element);
+    }
+}
