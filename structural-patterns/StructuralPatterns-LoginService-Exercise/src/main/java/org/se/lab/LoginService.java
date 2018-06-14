2211e103e9142d490099d524a8268b04166c9cb1
teiniker <egon.teiniker@gmail.com>
2018-06-14 12:36:02 +0200

    Moved examples.

--- structural-patterns/StructuralPatterns-LoginService-Exercise/src/main/java/org/se/lab/LoginService.java
+++
@@ -1,9 +0,0 @@
-package org.se.lab;
-
-public interface LoginService
-{
-	void addUser(int id, String username, String password, String mail);
-
-	boolean login(String username, String password);
-	void logout();
-}
